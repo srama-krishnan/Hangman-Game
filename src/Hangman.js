48ec97d0837aea2b209be7c3f3c1b9c2c6cd8cb7
S Ramakrishnan <sramakrishnan1841@gmail.com>
2025-06-28 18:01:27 +0530

    Stylized the display view

--- src/Hangman.js
+++ src/Hangman.js
@@ -77,7 +77,7 @@ class Hangman extends Component {
       <div className='Hangman'>
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} alt = {altText}/>
-        <p>Guessed Wrong: {this.state.nWrong}</p>
+        <p className="Hangman-status">Guessed Wrong: {this.state.nWrong}</p>
         <p className='Hangman-word'>{!gameOver ? this.guessedWord() : this.state.answer}</p>
         <p className='Hangman-btns'>
           {!gameOver
